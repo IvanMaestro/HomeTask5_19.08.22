e66d117c7c8c7367bffd22f1b235803427e51935
Masterov_IN <masterov_in@mail.ru>
2022-08-19 11:32:08 +0300

    Task36 done

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,4 +5,40 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
 
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]} ");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int OddIndexElemSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] arr = CreateArrayRndInt(8, -99, 99);
+PrintArray(arr);
+int result = OddIndexElemSum(arr);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных индексах = {result}");
